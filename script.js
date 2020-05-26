3b80dfc15ba205cddb6f12645c936532bd2e70cd
BubblyRobot <susyjustice@gmail.com>
2020-05-25 21:14:32 -0400

    added all the logic checks

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function writePassword() {
   // Here is where we are getting the length of the passwor from the user. 
   var length = prompt("Passwords must be between 8 and 128 characters. Please enter a number between 8 and 128.");
   while (length < 8  || length > 128){
-    var length = prompt("Please enter between 8 and 128 characteres");
+    var length = prompt("Please enter between 8 and 128 characters");
   }
   
   // Here is where are are gathering which type of characters to include in our password. 
   var number = confirm("Do you want to include numbers?");
   var special = confirm("Do you want to include special characters?");
-  var lower = confirm("Do you want to include special characters?")
+  var lower = confirm("Do you want to include lower case characters?");
   var upper = confirm("Do you want to include Upper Case characters?");
 
   //testing console logs below. 
@@ -38,18 +38,39 @@ function writePassword() {
       var passwordString = "";
       var password = "";
       var numbers = "0123456789"; 
+      // Need to add more special characters
       var specials = "@!#$%^&*()_+"; 
       var lowers = "abcdefghijklmnopqrstuvwxyz"; 
       var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
       
       //if else statements to build a string based on the user input. 
+      //I would be so happy if I had the knowledge to build a map and figure out neighbors and nodes.
+      //but alas I'm not that knowledgeable in javascript yet. 
       if ((number == true) && (special == false) && (lower == false) && (upper == false)){
         passwordString += numbers;
       }else if ((number == true) && (special == true) && (lower == false) && (upper == false)){
         passwordString += numbers += specials;
       } else if((number == true) && (special == true) && (lower == true) && (upper == false)){
-        passwordString += numbers += specials += lowers
-      }else{
+        passwordString += numbers += specials += lowers;
+      }else if ((number == false) && (special == false) && (lower == false) && (upper == true)){
+        passwordString += uppers;
+      }else if ((number == false) && (special == false) && (lower == true) && (upper == true)){
+        passwordString += lowers += uppers;
+      }else if ((number == false) && (special == true) && (lower == false) && (upper == false)){
+        passwordString += specials;
+      }else if ((number == false) && (special == false) && (lower == true) && (upper == false)){
+        passwordString += lowers;
+       }else if ((number == true) && (special == false) && (lower == false) && (upper == true)){
+         passwordString += numbers += uppers;
+      }else if ((number == false) && (special == true) && (lower == true) && (upper == false)){
+        passwordString += lowers += specials;
+      }else if ((number == false) && (special == true) && (lower == true) && (upper == false)){
+        passwordString += lowers += specials += uppers;
+      }else if ((number == false) && (special == true) && (lower == true) && (upper == false)){
+        passwordString += numbers += specials += uppers;
+      }else if ((number == true) && (special == false) && (lower == true) && (upper == true)){
+        passwordString += numbers += lowers += uppers;
+     }else{
         passwordString += numbers += specials += lowers += uppers;
       }
 
@@ -62,15 +83,18 @@ function writePassword() {
           //random number. Set to the length of our built string.  
         var j = Math.floor(Math.random() * passwordString.length);
 
-
+        //Selecting that character and building my string with it. 
         var k = passwordString.charAt(j);
 
         //var l = uppercase.charAt(j);
+      
         console.log(k);
-
+        // this is the password that will be returned to the main function. 
         password += k; 
+        
 
       }
+      //returning the password
       return password;
     }
     console.log("password: " + password);
